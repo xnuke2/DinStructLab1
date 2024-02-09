88d42c81a398b03573aed291eac153130c72bc4c
xnuke2 <xmnikit@gmail.com>
2024-02-09 14:59:13 +0300

    Добавьте файлы проекта.

---
+++ DinStructLab1/Lab1_2_Khmyrenkov.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <iostream>
+#include "SetHead.h"
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    Element* set = createRandomSet(rand() % (9-6+1)+6, 0, 20);
+    std::cout <<"Множество создано: "+ printSet(set, ' ') << std::endl;
+    std::cout << "Мощность множество: " + powerOfSet(set)<< std::endl;
+    std::cout << "Вывод множества: " + printSet(set, ' ') << std::endl;
+    std::cout << "Удаление множества" << std::endl;
+    set = deleteSet(set);
+    std::cout << "Мощность множество: " + powerOfSet(set) << std::endl;
+    std::cout << "Вывод множества: " + printSet(set, ' ') << std::endl;
+    deleteSet(createRandomSet(10, 0, 100));
+}
---
+++ DinStructLab1/SetHead.h
@@ -0,0 +1,109 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+struct Element
+{
+	int Value;
+	Element* Next;
+};
+
+Element* createEmptySet() {
+	Element* firstElement = NULL;
+	return firstElement;
+}
+
+bool setIsEmpty(Element* firstElement) {
+	if (firstElement == NULL) {
+		return true;
+	}
+	return false;
+}
+
+bool setContains(Element* firstElement,int numToFind) {
+	Element* tmp = firstElement;
+	while (tmp!=NULL)
+	{
+		if (tmp->Value == numToFind) {
+			return true;
+		}
+		tmp = tmp->Next;
+	}
+	return false;
+}
+
+Element* setAddFirst(Element* firstElement, int num) {
+	if (setContains(firstElement, num)) {
+		return firstElement;
+	}
+	Element* newStart = new Element;
+	newStart->Value = num;
+	newStart->Next = firstElement;
+	return newStart;
+}
+
+Element* createRandomSet(int length, int min, int max) {
+	Element* newSet = createEmptySet();
+	if (length <= 0|| max - min < length) {
+		return newSet;
+	}
+	for (int i = 0; i < length; i++) {
+		int newNum = rand() % (max - min + 1) + min;
+		while (setContains(newSet, newNum)) {
+			newNum = rand() % (max - min + 1) + min;
+		}
+		newSet = setAddFirst(newSet, newNum);
+	}
+	return newSet;
+}
+
+int powerOfSet(Element* firstElement) {
+	Element* tmp = firstElement;
+	int length = 0;
+	while (tmp != NULL)
+	{
+		length++;
+		tmp = tmp->Next;
+	}
+	return length;
+}
+
+std::string printSet(Element* firstElement, char separator) {
+	std::string rezult = "";
+	if (setIsEmpty(firstElement)) {
+		return rezult;
+	}
+	Element* tmp = firstElement;
+	while (tmp != NULL) {
+		rezult = rezult + std::to_string(tmp->Value);
+		if (tmp->Next != NULL) {
+			rezult = rezult + separator;
+		}
+		tmp = tmp->Next;
+	}
+	return rezult;
+}
+
+Element* deleteSet(Element* firstElement) {
+	if (setIsEmpty(firstElement)) {
+		return firstElement;
+	}
+	Element* tmp = firstElement;
+	Element* tmpLast = firstElement;
+	while(tmp!=NULL){
+		while (tmp->Next != NULL) {
+			tmpLast = tmp;
+			tmp = tmp->Next;
+		}
+		if (tmp != tmpLast) {
+			tmpLast->Next = NULL;
+		}
+		if (tmp == firstElement) {
+			firstElement = NULL;
+		}
+		delete tmp;
+		tmp = NULL;
+		tmpLast = tmp = firstElement;
+	}
+	return firstElement;
+}
